b66ff5fdf129a9507b9aa9651f9c11396e662a4c
Árpád Kiss <arpad1019@gmail.com>
2012-10-21 19:15:46 +0200

    Initial Commit

---
+++ CompTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CompTest
+//
+//  Created by Árpád Kiss on 2012.10.21..
+//  Copyright (c) 2012 Uni-Obuda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
